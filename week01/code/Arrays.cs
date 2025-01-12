40d7612064f52ce069e2c5f65cb212a8b827b2c6
redheadw <wrightinthered@att.net>
2025-01-12 16:24:21 -0600

    arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 public static class Arrays
 {
     /// <summary>
@@ -12,8 +14,15 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+       // create an array to store the multiples
+       double[] multiples = new double[length];
 
-        return []; // replace this return statement with your own
+       // put multiples of the number in the array
+       for (int i = 0; i < length; i++)
+       {
+            multiples[i] = number * (i + 1);
+       }
+        return multiples; // awnser 
     }
 
     /// <summary>
@@ -29,5 +38,16 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        // needs to be the size of the array
+        amount %= data.Count;
+        // list has 2 parts: lastPart, firstPart
+        List<int> lastPart = data.GetRange(data.Count - amount, amount);
+        List<int> firstPart = data.GetRange(0, data.Count - amount);
+
+        // clear old list and create the new list
+        data.Clear();
+        data.AddRange(lastPart);
+        data.AddRange(firstPart);
+        
     }
 }
